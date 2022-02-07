165779ce650e2201332da4f9b1cb78392c229987
ggi88 <gimrandy@gmail.com>
2022-02-07 19:21:03 +0900

    detail page update

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
+
+export default function Potato() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>about us</h1>
+    </div>
+  );
+}
